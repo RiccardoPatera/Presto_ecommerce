bdaf86ac1af30c6ab450773eafdf60f2d838bfb8
Miguel De la Vega <miguelangel.dlvn@gmail.com>
2023-06-07 12:27:56 +0200

    sortby e inizio dropdown

--- resources/views/Articles/items.blade.php
+++ resources/views/Articles/items.blade.php
@@ -1,17 +1,26 @@
 <x-layout>
+
     
+    {{-- <div class="dropdown">
+        <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Dropdown button
+        </button>
+        <ul class="dropdown-menu">
+            @foreach ($category->$articles as $article)
+            <li><a class="dropdown-item" href=""></a></li>
+            @endforeach
+        </ul>
+    </div>    --}}
+    
+
+
     <div class="container-fluid">
         <div class="row ">
             @foreach ($articles as $article)
             <div class="col-12 col-md-6 d-flex justify-content-center">
-                
                     @livewire('item-card', compact('article'))
-               
             </div>
             @endforeach
         </div>
-         
-    
-    </div>
-    
+    </div>   
 </x-layout>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,11 @@
 <x-layout>
-
-    
-
+    <div class="container-fluid">
+        <div class="row ">
+            @foreach ($articles as $article)
+            <div class="col-12 col-md-6 d-flex justify-content-center">
+                    @livewire('item-card', compact('article'))
+            </div>
+            @endforeach
+        </div>
+    </div>   
 </x-layout>
