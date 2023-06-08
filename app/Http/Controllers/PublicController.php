2e931bc96efa9f6f25ec045fccf6a8844c2474f0
Riccardo Giovanni Patera <paterariccardo@gmail.com>
2023-06-08 18:37:39 +0200

    revisor

--- app/Http/Controllers/PublicController.php
+++ app/Http/Controllers/PublicController.php
@@ -10,8 +10,6 @@ class PublicController extends Controller
     public function welcome() {
         $articles_all=Article::all();
         $articles_all=Article::orderBy('created_at', 'DESC')->where('is_accepted',true)->get();
-        // $articles=$articles_all->where('is_accepted',true);
-        // $articles = Article::take(6)->orderBy('created_at', 'DESC')->get();
         return view('welcome', ['articles'=>$articles_all]);
     }
 
