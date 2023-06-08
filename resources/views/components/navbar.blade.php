81dbaee30a6bc236b86c59961e81a4eb3f17c88f
Miguel De la Vega <miguelangel.dlvn@gmail.com>
2023-06-08 12:59:08 +0200

    ulteriore fixnav

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -48,14 +48,14 @@
                 @endauth
                 
             </ul>
-        </div>
-    </div>
-    <div>
-                <form class="d-flex" role="search">
+            <form class="d-flex" role="search">
                 <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
                 <button class="btn btn-outline-dark" type="submit"><i class="fa-solid fa-magnifying-glass"></i></button>
             </form>
-            </div>
+          
+        </div>
+    </div>
+                
 </nav>
 
 <script>
