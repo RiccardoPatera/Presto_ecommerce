a26efa62ffad7b0ca76c4163cb9a419dfced4af9
Riccardo Giovanni Patera <paterariccardo@gmail.com>
2023-06-12 09:10:09 +0200

    Middleware update

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -15,7 +15,7 @@ class ArticleController extends Controller
 
     public function __construct()
     {
-        $this->middleware('auth')->except(['index','show']);
+        $this->middleware('auth')->except(['index','show','search']);
 
 
     }
