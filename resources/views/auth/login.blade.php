8ea2b03c436b1f0f21a61ec47c209a1f496c57ea
Miguel De la Vega <miguelangel.dlvn@gmail.com>
2023-06-13 00:34:45 +0200

    fix colors

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,7 +3,7 @@
     <div class="container">
         <div class="row">
             <div class="col-12">
-                <h1 class="text-center display-1 my-5">Login</h1>
+                <h1 class="text-center display-1 my-5 text-light">Login</h1>
             </div>
         </div>
     </div>
@@ -15,13 +15,13 @@
                     @csrf
 
                     <div class="mb-3">
-                        <label for="userMail" class="form-label">Email</label>
+                        <label for="userMail" class="form-label text-light">Email</label>
                         <input type="email" class="form-control" name="email" id="userMail" aria-describedby="emailHelp">
                     </div>
 
 
                     <div class="mb-3">
-                        <label for="password" class="form-label">Password</label>
+                        <label for="password" class="form-label text-light">Password</label>
                         <input type="password" class="form-control" name="password" id="password">
                     </div>
                     @error('email')
@@ -33,9 +33,9 @@
 
 
 
-                    <p>Not Register? <a href="{{route('register')}}">Register Here</a></p>
+                    <p class=" text-light">Not Register? <a href="{{route('register')}}" class=" text-light">Register Here</a></p>
 
-                    <button type="submit" class="btn btn-outline-dark">Login</button>
+                    <button type="submit" class="btn btn-outline-dark text-light">Login</button>
 
                 </form>
             </div>
