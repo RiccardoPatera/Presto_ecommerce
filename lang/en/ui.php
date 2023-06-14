fae22ab9dc61fde4baee8861c5952780631cebff
Alessio Rimedio <alessio.rimedio@silencio.digital>
2023-06-14 19:32:18 +0200

    lang

--- lang/en/ui.php
+++ lang/en/ui.php
@@ -38,6 +38,7 @@ return[
     'reject'=>'Reject',
     'singleCat'=>'Category',
     'acceptArt'=>'You`ve accepted the article',
+    'rejectArt'=>'You`ve accepted the article',
     'createMess'=>'Article created successfully. Wait for the revisor to accept it.',
     'thankYou'=>'Thank you! Your candidature is under review',
     'allCat'=>'All categories',
@@ -61,5 +62,6 @@ return[
     'reqEmail'=>'The email is required',
     'reqPass'=>'The password is required',
     'ourvision'=>'"The vision for a vintage usage shop is to create a space where the beauty, nostalgia, and craftsmanship of bygone eras are celebrated and repurposed for modern-day use. It aims to provide a curated collection of unique and high-quality vintage items that can be integrated seamlessly into contemporary lifestyles. The shop promotes sustainability by encouraging the reuse and upcycling of vintage pieces, allowing customers to infuse their surroundings with character and history. It aspires to be a hub for creativity and inspiration, where individuals can find one-of-a-kind treasures that tell stories and evoke a sense of timelessness."',
-    'ourv'=> 'Our vision'
+    'ourv'=> 'Our vision',
+    
 ];
--- lang/es/ui.php
+++ lang/es/ui.php
@@ -38,6 +38,7 @@ return[
     'reject'=>'Niega',
     'singleCat'=>'Categoria',
     'acceptArt'=> 'Has aceptado el articulo',
+    'rejectArt'=>'Has rechazado el articulo',
     'createMess'=>'Tu articulo ha sido creado correctamente. Espera que un revisor acepte tu articulo ',
     'thankYou'=>'Gracias tu solicitud esta por ser revisada',
     'allCat'=>'Todas las categorias',
@@ -62,4 +63,5 @@ return[
     'reqPass'=>'Se requiere la contrasena',
     'ourvision'=>'"La visión de una tienda de uso de vintage es crear un espacio donde se celebre la belleza, la nostalgia y la artesanía de eras pasadas, dándoles un nuevo propósito en el uso moderno. Se busca ofrecer una cuidada colección de piezas vintage únicas y de alta calidad que se integren sin problemas en los estilos de vida contemporáneos. La tienda promueve la sostenibilidad al fomentar la reutilización y el reciclaje creativo de artículos vintage, permitiendo a los clientes infundir carácter e historia en sus entornos. Aspira a ser un centro de creatividad e inspiración, donde las personas puedan encontrar tesoros únicos que cuenten historias y evocan una sensación de atemporalidad."',
     'ourv'=> 'Nuestra visiòn',
+    'rejectArt'=>'Has rechazado el articulo'
 ];
