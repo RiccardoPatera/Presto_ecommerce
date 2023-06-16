313927b669e609ca5717199f01ac637d542c41fd
Riccardo Giovanni Patera <paterariccardo@gmail.com>
2023-06-16 10:13:50 +0200

    piccole modifiche

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,7 +1,7 @@
 <x-layout>
 
-        <div class="container bg-light user  rounded">
-            <div class="row justify-content-center">
+        <div class="container bg-light user  rounded p-0">
+            <div class="row justify-content-center p-0">
                 <div class="col-12">
                     <h1 class="text-center display-1 my-5 ">{{__('ui.login')}}</h1>
                 </div>
@@ -10,7 +10,7 @@
 
 
 
-                <div class="col-12 col-md-6">
+                <div class="col-12 col-md-6 p-0">
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
 
@@ -36,7 +36,7 @@
 
                         <p class=" ">{{__('ui.notReg')}} <a href="{{route('register')}}" class="text-black">{{__('ui.hereReg')}}</a></p>
 
-                        <button type="submit" class="btn btn-dark mb-2">{{__('ui.login')}}</button>
+                        <button type="submit" class="btn btn-dark mb-4">{{__('ui.login')}}</button>
 
                     </form>
                 </div>
