f27756f187efe4a9596fb866ad06f98c800aab08
Luca Domenico Di Giorgio <luke.096@icloud.com>
2023-06-17 15:32:27 +0200

    fix

--- resources/views/user/dashboard.blade.php
+++ resources/views/user/dashboard.blade.php
@@ -1,8 +1,8 @@
-<x-layout>
+{{-- <x-layout>
     <div class="container-fluid py-5">
         <div class="row d-flex justify-content-between">
             <div class="col-12 d-flex justify-content-center">
-                <h1 class="text-light text-center">Dashboard</h1>
+                <h1 class="text-light text-center display-1">Dashboard</h1>
             </div>
 
             <div class="row">
@@ -13,12 +13,12 @@
                 @endif
                 <div class="col-12 col-md-6 my-5 vh-50">
                     <div class="row justify-content-center">
-                        <div class="col-12">
-                            <h2 class="text-white text-center">User info</h2>
+                        <div class="col-6 mb-3 me-3">
+                            <p class="text-start text-light fs-3">User info</p>
                         </div>
                         <div class="col-9 my-3">
-                            <div class="card card-detail  shadow p-5 d-flex justify-content-center bg-light ">
-
+                            <div class="card card-userInfo p-5">
+                                
                                 <h3>{{ Ucwords($user->name) }}</h3>
                                 <p>{{ $user->email }}</p>
                                 <hr class="hr">
@@ -26,16 +26,21 @@
                                 <h5>Articols uploaded: {{ count($user->articles) }}</h5>
                                 <h5>Articles accepted: {{ count($user->articles->where('is_accepted', 1)) }}</h5>
                                 <h5>Iscritto il: {{ $user->created_at }}</h5>
-                                {{-- <a class="btn btn-outline-dark" href="#">Add to cart</a> --}}
+
+                                <a class="btn btn-outline-dark" href="#">Add to cart</a>
+
                             </div>
                         </div>
                     </div>
                 </div>
 
-                <div class="col-12 col-md-6 my-5 vh-50">
+
+                <div class="col-12 bg-success col-md-6 mt-5 vh-50">
+
+
                     <div class="row">
                         <div class="col-12">
-                            <h2 class="text-white text-center"> User Articles</h2>
+                            <h2 class="text-white text-center mb-5"> User Articles</h2>
                         </div>
 
                         <div class="col-12">
@@ -44,16 +49,17 @@
                                 <div class="swiper-wrapper">
                                     @forelse ($user->articles as $article)
                                         <div class="swiper-slide">
-                                            <a class="" href="{{ route('show_article', compact('article')) }}">
-                                                <div class="card">
+                                            <div class="card">
+                                                <a href="{{ route('show_article', compact('article')) }}">
                                                     <div class="fadex"></div>
                                                     <img src="{{ $article->images()->first()->GetUrl(500, 500) }}"
-                                                        class="card-img-top img-fluid " alt="...">
+                                                        class="card-img-top img-fluid rounded" alt="...">
                                                     <div class="card-info ms-3 mt-3">
                                                         <p class="text-light fst-italic mb-2">{{ $article->category->category }}</p>
                                                         <h5 class="text-light fw-bold text mb-0">{{ $article->title }}</h5>
                                                         <p class="text-light fst-italic">{{ $article->price }} €</p>
                                                     </div>
+                                                </a>
 
                                                     @if ($article->is_accepted === null)
 
@@ -63,14 +69,14 @@
 
                                                     @elseif($article->is_accepted == 1)
 
-                                                        <div class="bg-dark bg-opacity-75 rounded-end position-absolute start-0 bottom-0 pt-1 px-3">
+                                                        <div class="bg-dark bg-opacity-75 rounded-custom position-absolute start-0 bottom-0 pt-1 px-3">
                                                             <h6 class="text-accepted text-start mt-1 me-4">Accepted</h6>
                                                         </div>
 
-                                                        {{-- Edit-Delete --}}
+                                                        Edit-Delete
                                                         <div class="fs-5 text-light position-absolute bottom-0 end-0 translate-middle-x">
-                                                            <button class="btn bg-dark p-0" href="{{route('edit_article', compact('article'))}}"><i class="fa-solid fa-pencil textEdit bg-opacity-75 rounded-top p-2"></i></button>
-                                                            <button class="btn bg-dark p-0" href=""><i class="fa-solid fa-trash textDelete  bg-opacity-75 rounded-top p-2"></i></button>
+                                                            <a class="btn bg-dark p-0" href="{{route('edit_article', compact('article'))}}"><i class="fa-solid fa-pencil textEdit bg-opacity-75 rounded-top p-2"></i></a>
+                                                            <a class="btn bg-dark p-0" href=""><i class="fa-solid fa-trash textDelete  bg-opacity-75 rounded-top p-2"></i></a>
                                                         </div>
 
                                                     @elseif($article->is_accepted == 0)
@@ -79,15 +85,14 @@
                                                             <h6 class="text-rejected text-start mt-1 me-4">Rejected</h6>
                                                         </div>
 
-                                                        {{-- Edit-Delete --}}
+                                                        Edit-Delete
                                                         <div class="fs-5 text-light position-absolute bottom-0 end-0 translate-middle-x">
-                                                            <button class="btn bg-dark p-0" href="{{route('edit_article', compact('article'))}}"><i class="fa-solid fa-pencil textEdit bg-opacity-75 rounded-top p-2"></i></button>
-                                                            <button class="btn bg-dark p-0" href=""><i class="fa-solid fa-trash textDelete  bg-opacity-75 rounded-top p-2"></i></button>
+                                                            <a class="btn bg-dark p-0" href="{{route('edit_article', compact('article'))}}"><i class="fa-solid fa-pencil textEdit bg-opacity-75 rounded-top p-2"></i></a>
+                                                            <a class="btn bg-dark p-0" href=""><i class="fa-solid fa-trash textDelete  bg-opacity-75 rounded-top p-2"></i></a>
                                                         </div>
                                                     @endif
 
                                                 </div>
-                                            </a>
                                         </div>
                                     @empty
                                         <div class="col-12  d-flex align-item-center justify-content-center">
@@ -102,4 +107,31 @@
             </div>
         </div>
     </div>
+</x-layout> --}}
+
+
+<x-layout>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-12 d-flex justify-content-center">
+                <h1 class="text-light text-center display-1">Dashboard</h1>
+            </div>
+        </div>
+    </div>
+
+    @if (session('message'))
+        <div class="col-12 ">
+            <p class="alert alert-success text-center">{{ session('message') }}</p>
+        </div>
+    @endif
+
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+
+            </div>
+        </div>
+    </div>
+
 </x-layout>
