0ca2e8000b1f00ef7d7a921d159de23c4ae6ddae
Luca Domenico Di Giorgio <luke.096@icloud.com>
2023-06-19 12:08:12 +0200

    fix

--- resources/views/livewire/users-profile.blade.php
+++ resources/views/livewire/users-profile.blade.php
@@ -8,15 +8,14 @@
                 <div class="col-6 d-flex flex-column align-items-center justify-content-center">
                     <img src="{{Storage::url($user->img)}}" alt="user_img" class="img-fluid user-img ">
                 </div>
-                <div class="col-6 d-flex flex-column align-items-center justify-content-center ">
+                <div class="col-6 d-flex flex-column align-items-center justify-content-center">
                     <h6 class="">{{count($user->articles)}} Articles Uploaded</h6>
-
                 </div>
             </div>
         </div>
 
 
-        <div class="col-12  d-flex flex-column justify-content-center  user-articles">
+        <div class="col-12  d-flex flex-column justify-content-center user-articles">
             <h3 class="text-center text-light ">User Articles</h3>
             <div class="swiper mySwiper">
                 <div class="swiper-wrapper">
@@ -26,10 +25,10 @@
                                 <a href="{{ route('show_article', compact('article')) }}">
                                     <div class="fadex"></div>
                                     <img src="{{ $article->images()->first()->GetUrl(500, 500) }}"
-                                        class="card-img-top img-fluid rounded" alt="...">
+                                        class="card-img-top img-fluid rounded-card" alt="...">
                                     <div class="card-info ms-3 mt-3">
-                                        <p class="text-light fst-italic mb-2">{{ $article->category->category }}</p>
-                                        <h5 class="text-light fw-bold text mb-0">{{ $article->title }}</h5>
+                                        <p class="text-light fst-italic mb-1">{{ $article->category->category }}</p>
+                                        <h5 class="text-light fw-bold text mb-1">{{ $article->title }}</h5>
                                         <p class="text-light fst-italic">{{ $article->price }} €</p>
                                     </div>
                                 </a>
