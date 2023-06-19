fdc926b5f1d522e6241d0cc26d415b8ccf920242
Riccardo Giovanni Patera <paterariccardo@gmail.com>
2023-06-19 12:57:43 +0200

    fix minori

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,62 +1,63 @@
 <x-layout>
 
     <div class="container">
-        <div class="row justify-content-center vh-100 align-items-center ">
+        <div class="row justify-content-center min-100 align-items-center ">
 
 
             <div class="col-12 col-md-8 user rounded bg-light p-5">
                 <div class="row">
-                    <div class="col-12 col-md-12">
+                    <div class="col-12 ">
                         <h1 class="text-center display-1 mb-5 mt-3">{{__('ui.register')}}</h1>
                     </div>
+                    <div class="col-12">
+                        <form method="POST" action="{{ route('register') }}" clas >
+                            @csrf
 
-                    <form method="POST" action="{{ route('register') }}" clas >
-                        @csrf
+                            <div class="mb-3">
+                                <label for="userName" class="form-label ">{{__('ui.user')}}</label>
+                                <input type="text" class="form-control @error('name') is-invalid @enderror" name="name" id="userName"
+                                    aria-describedby="emailHelp"  value={{old('name')}}>
+                            </div>
+                             @error('name')
+                                <div class="alert alert-danger">{{$message}}</div>
+                            @enderror
 
-                        <div class="mb-3">
-                            <label for="userName" class="form-label ">{{__('ui.user')}}</label>
-                            <input type="text" class="form-control @error('name') is-invalid @enderror" name="name" id="userName"
-                                aria-describedby="emailHelp">
-                        </div>
-                        @error('name')
-                            <div class="alert alert-danger">{{$message}}</div>
-                        @enderror
+                            <div class="mb-3">
+                                <label for="userMail" class="form-label ">Email</label>
+                                <input type="email" class="form-control @error('email') is-invalid @enderror" name="email" id="userMail"
+                                    aria-describedby="emailHelp" value={{old('email')}}>
+                            </div>
 
-                        <div class="mb-3">
-                            <label for="userMail" class="form-label ">Email</label>
-                            <input type="email" class="form-control @error('email') is-invalid @enderror" name="email" id="userMail"
-                                aria-describedby="emailHelp">
-                        </div>
+                            @error('email')
+                                <div class="alert alert-danger">{{$message}}</div>
+                            @enderror
 
-                        @error('email')
-                            <div class="alert alert-danger">{{$message}}</div>
-                        @enderror
+                            <div class="mb-3">
+                                <label for="password" class="form-label ">{{__('ui.password')}}</label>
+                                <input type="password" class="form-control @error('password') is-invalid @enderror" name="password" id="password" >
+                            </div>
 
-                        <div class="mb-3">
-                            <label for="password" class="form-label ">{{__('ui.password')}}</label>
-                            <input type="password" class="form-control @error('password') is-invalid @enderror" name="password" id="password">
-                        </div>
+                            @error('password')
+                                <div class="alert alert-danger">{{$message}}</div>
+                            @enderror
 
-                        @error('password')
-                            <div class="alert alert-danger">{{$message}}</div>
-                        @enderror
+                            <div class="mb-3">
+                                <label for="passwordConfirmation" class="form-label ">{{__('ui.confPass')}}</label>
+                                <input type="password" name="password_confirmation" class="form-control @error('password') is-invalid @enderror"
+                                    id="passwordConfirmation">
+                            </div>
 
-                        <div class="mb-3">
-                            <label for="passwordConfirmation" class="form-label ">{{__('ui.confPass')}}</label>
-                            <input type="password" name="password_confirmation" class="form-control @error('password') is-invalid @enderror"
-                                id="passwordConfirmation">
-                        </div>
-
-                        @error('password')
-                            <div class="alert alert-danger">{{$message}}</div>
-                        @enderror
+                            @error('password')
+                                <div class="alert alert-danger">{{$message}}</div>
+                            @enderror
 
 
-                        <p class=" ">{{__('ui.alrReg')}} <a href="{{route('login')}}" class="text-dark ">{{__('ui.hereLog')}}</a></p>
+                            <p class=" ">{{__('ui.alrReg')}} <a href="{{route('login')}}" class="text-dark ">{{__('ui.hereLog')}}</a></p>
 
-                        <button type="submit" class="btn btn-dark mb-5">{{__('ui.register')}}</button>
+                            <button type="submit" class="btn btn-dark mb-5">{{__('ui.register')}}</button>
 
-                    </form>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
